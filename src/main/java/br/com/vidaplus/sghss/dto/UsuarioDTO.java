ac42f6662a992486fb0dfe745cef7ab2cd027d2c
Patricky Lucas <patrickylucas@hotmail.com>
2025-06-16 22:27:50 -0300

    adicionando informação ao swagger nos dtos

--- src/main/java/br/com/vidaplus/sghss/dto/UsuarioDTO.java
+++ src/main/java/br/com/vidaplus/sghss/dto/UsuarioDTO.java
@@ -1,5 +1,6 @@
 package br.com.vidaplus.sghss.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -16,16 +17,23 @@ import lombok.Setter;
 @Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(
+        description = "DTO para representar um usuário no sistema",
+        title = "Usuário DTO")
 public class UsuarioDTO {
     @NotBlank(message = "O nome de usuário é obrigatório")
     @Size(min = 4, max = 50, message = "O nome de usuário deve ter entre 4 e 50 caracteres")
+    @Schema(description = "Nome de usuário para autenticação", example = "usuario123")
     private String username;
 
     @NotBlank(message = "A senha é obrigatória")
     @Size(min = 6, message = "A senha deve ter pelo menos 6 caracteres")
+    @Schema(description = "Senha para autenticação do usuário", example = "senhaSegura123")
     private String password;
 
     @NotBlank(message = "O nome do papel é obrigatório")
+    @Size(max = 50, message = "O nome do papel deve ter no máximo 50 caracteres")
+    @Schema(description = "Nome do papel ou função do usuário no sistema", example = "ADMIN/MEDICO/PACIENTE")
     private String roleNome;
 
 }
--- src/main/java/br/com/vidaplus/sghss/dto/request/ProfissionalSaudeComUsuarioRequestDTO.java
+++ src/main/java/br/com/vidaplus/sghss/dto/request/ProfissionalSaudeComUsuarioRequestDTO.java
@@ -1,6 +1,7 @@
 package br.com.vidaplus.sghss.dto.request;
 
 import br.com.vidaplus.sghss.dto.UsuarioDTO;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,9 +16,14 @@ import lombok.Setter;
 @Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(
+        description = "DTO para requisição de Profissional de Saúde com informações de usuário",
+        title = "Profissional de Saúde com Usuário Request DTO")
 public class ProfissionalSaudeComUsuarioRequestDTO {
     @Valid
+    @Schema(description = "Informações do profissional de saúde")
     private ProfissionalSaudeRequestDTO profissional;
     @Valid
+    @Schema(description = "Informações do usuário associado ao profissional de saúde")
     private UsuarioDTO usuario;
 }
