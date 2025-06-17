bab6b44c3c5c932e292bb82ee825a5e19a4843d9
Patricky Lucas <patrickylucas@hotmail.com>
2025-06-16 22:09:21 -0300

    adicionando informação ao swagger de openconfig

--- src/main/java/br/com/vidaplus/sghss/config/OpenApiConfig.java
+++ src/main/java/br/com/vidaplus/sghss/config/OpenApiConfig.java
@@ -1,7 +1,9 @@
 package br.com.vidaplus.sghss.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -30,7 +32,16 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("VidaPlus SGHSS API")
                         .version("1.0")
-                        .description("Documentação da API do Sistema de Gestão Hospitalar e Serviços de Saúde (SGHSS) do VidaPlus"))
+                        .description("API desenvolvida para o Projeto Multidisciplinar da Uninter do curso de Análise e Desenvolvimento de Sistemas como parte do Sistema de Gestão Hospitalar e Serviços de Saúde (SGHSS) da plataforma VidaPlus. " +
+                                "Fornece uma interface RESTful para operações críticas, incluindo autenticação com JWT, gerenciamento de usuários com controle de acesso baseado em papéis (RBAC), cadastro e manutenção de entidades como pacientes, profissionais de saúde, consultas, prontuários eletrônicos e auditorias. " +
+                                "Projetada com foco em segurança, escalabilidade e conformidade com requisitos de sistemas hospitalares modernos.")
+                        .contact(new Contact()
+                            .name("Patricky Lucas")
+                            .email("patrickylucas@hotmail.com")
+                            .url("https://github.com/PatrickyLucas/vida-plus-sghss"))
+                        .license(new License()
+                            .name("MIT License")
+                            .url("https://opensource.org/licenses/MIT")))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(new io.swagger.v3.oas.models.Components()
                         .addSecuritySchemes(securitySchemeName,
